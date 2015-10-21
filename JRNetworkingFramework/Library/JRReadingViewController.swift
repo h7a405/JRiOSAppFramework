1355f1308fc89c580c69b7636034a5cbcab06913
SilversRayleigh <SilversRayleigh@Rayleighs-Mac-mini.local>
2015-10-21 17:13:22 +0800

    reading view logic updating

--- JRNetworkingFramework/Library/JRReadingViewController.swift
+++ JRNetworkingFramework/Library/JRReadingViewController.swift
@@ -29,8 +29,10 @@ class JRReadingViewController: UIViewController {
                 }
                 UIView.animateWithDuration(0.2, animations: {()
                     self.toolBar.transform = CGAffineTransformTranslate(self.toolBar.transform, 0, yToTransform)
-                }, completion: {(finished: Bool) in
-                    self.navigationController?.navigationBarHidden = newValue
+                    
+                    }, completion: {(finished: Bool) in
+                        
+                        self.navigationController?.navigationBarHidden = newValue
                 })
             }
         }
@@ -46,6 +48,22 @@ class JRReadingViewController: UIViewController {
             }
         }
     }
+    
+    var hasLast: Bool = false
+    var hasNext: Bool = true
+    
+    var currentPage: Int = 0 {
+        willSet {
+            if newValue <= 0 {
+                self.hasLast = false
+                self.hasNext = true
+            } else if newValue >= self.totalPages {
+                self.hasLast = true
+                self.hasNext = false
+            }
+        }
+    }
+    var totalPages: Int = 9
     //MARK: Parameters - Foundation
     //MARK: Parameters - UIKit
     @IBOutlet weak var contentScrollView: UIScrollView!
@@ -57,7 +75,6 @@ class JRReadingViewController: UIViewController {
     var currentReadingView: JRReadingView?
     var lastReadingView: JRReadingView?
     var nextReadingView: JRReadingView?
-    var cacheReadingView: JRReadingView?
     
     //MARK: - Methods
     //MARK: Methods - Override
@@ -67,7 +84,7 @@ class JRReadingViewController: UIViewController {
         self.contentScrollView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "didContentScrollViewTouched:"))
         
         self.readingViews = Array()
-        for _ in 0..<4 {
+        for _ in 0..<3 {
             self.readingViews!.append(JRReadingView())
         }
     }
@@ -106,9 +123,15 @@ extension JRReadingViewController {
         if self.readingViews != nil {
             for i in 0..<self.readingViews!.count {
                 readingViews![i].frame = CGRect(x: CGFloat(i) * self.contentScrollView.frame.size.width, y: 0, width: self.contentScrollView.frame.size.width, height: self.contentScrollView.frame.size.height)
+                readingViews![i].backgroundColor = UIColor.RGB(red: CGFloat(i) * 50, green: 100, blue: 100)
+                self.contentScrollView.addSubview(readingViews![i])
             }
         }
-        self.contentScrollView.contentSize = CGSize(width: CGFloat(self.readingViews!.count - 1) * self.contentScrollView.frame.size.width, height: self.contentScrollView.frame.size.height)
+        self.contentScrollView.contentSize = CGSize(width: CGFloat(self.readingViews!.count) * self.contentScrollView.frame.size.width, height: self.contentScrollView.frame.size.height)
+//        self.contentScrollView.setContentOffset(CGPoint(x: CGFloat(Int(self.readingViews!.count)) * self.contentScrollView.frame.size.width, y: 0), animated: false)
+        self.lastReadingView = self.readingViews![(currentPage + 2) % 3]
+        self.currentReadingView = self.readingViews![currentPage % 3]
+        self.nextReadingView = self.readingViews![(currentPage + 1) % 3]
     }
 }
 //MARK: Extensions - Operation & Action
@@ -116,7 +139,7 @@ extension JRReadingViewController {
     func didContentScrollViewTouched(sender: UITapGestureRecognizer) {
         let endOfLeftZone: CGFloat = UIScreen.mainScreen().bounds.width * CGFloat(0.2)
         let beginOfRightZone: CGFloat = UIScreen.mainScreen().bounds.width * CGFloat(0.8)
-        let touchedPoint: CGPoint = sender.locationInView(self.contentScrollView)
+        let touchedPoint: CGPoint = sender.locationInView(self.view)
         if touchedPoint.x < endOfLeftZone {
             self.loadNextPage(isDirectionRight: false)
         } else if touchedPoint.x > beginOfRightZone {
@@ -126,7 +149,36 @@ extension JRReadingViewController {
         }
     }
     func loadNextPage(isDirectionRight direction: Bool) {
-        
+        if direction {
+            if self.hasNext {
+                self.changeCurrentReadingView(true)
+            } else {
+                SVProgressHUD.showInfoWithStatus("已到最后一页")
+            }
+        } else {
+            if self.hasLast {
+                self.changeCurrentReadingView(false)
+            } else {
+                SVProgressHUD.showInfoWithStatus("已到第一页")
+            }
+        }
+    }
+    func changeCurrentReadingView(isNext: Bool) {
+        if isNext {
+            self.lastReadingView = self.currentReadingView
+            self.currentReadingView = self.nextReadingView
+//            self.currentPage = (self.currentPage + 1) % 3
+            self.currentPage = self.currentPage + 1
+            self.nextReadingView = self.readingViews![self.currentPage % 3]
+        } else {
+            self.nextReadingView = self.currentReadingView
+            self.currentReadingView = self.lastReadingView
+//            self.currentPage = (self.currentPage - 1) % 3
+            self.currentPage = self.currentPage - 1
+            self.lastReadingView = self.readingViews![self.currentPage % 3]
+        }
+        self.contentScrollView.setContentOffset(self.currentReadingView!.frame.origin, animated: true)
+        Log.VLog(self.currentPage)
     }
 }
 //MARK: Extensions - Getter / Setter
